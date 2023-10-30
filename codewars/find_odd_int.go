c4f47cc3f957083eedf15819a38d6ad5765aa06b
Jack <>
2023-10-29 20:58:28 -0400

    cleaned up
    Signed-off-by: Jack <>

--- codewars/find_odd_int.go
+++ codewars/find_odd_int.go
@@ -3,25 +3,19 @@
 
 package main
 
-import "fmt"
-
 func FindOdd(seq []int) int {
     count := make(map[int]int)
 
 	for _,v := range seq {
 		if count[v] == 0{
-			//fmt.Println("Value not in count, adding...")
 			count[v] = 1
 		} else {
-			//fmt.Println("Value in count, increasing...")
-			count[v] += 1
+			delete(count, v)
 		}
 	}
 
-	for n,c := range count {
-		if c % 2 == 1 {
-			return n
-		}
+	for n,_ := range count {
+		return n
 	}
 
     return 0
@@ -36,10 +30,8 @@ func main () {
         {1,1,1,1,1,1,10,1,1,1,1},
         {5,4,3,2,1,5,4,3,2,10,10},
     }
-    sol := [...]int{5,-1,5,10,10,1}
-    for i,v := range arr {
-        fmt.Sprintf("Testing input %v",v)
-        fmt.Println("Answer should be:", sol[i])
-        fmt.Println(FindOdd(v))
+    //sol := [...]int{5,-1,5,10,10,1}
+    for _,v := range arr {
+    	FindOdd(v)
     }
 }
